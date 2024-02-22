af48be32c4fe3c11afb2748f6e54eca3498811af
Alec Lima Rondel <alecrondel@hotmail.com>
2024-02-21 21:23:08 -0300

    first commit

---
+++ src/main/java/ua/lec/springbootlab4/ProjetoLab4Application.java
@@ -0,0 +1,13 @@
+package ua.lec.springbootlab4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoLab4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoLab4Application.class, args);
+	}
+
+}
---
+++ src/test/java/ua/lec/springbootlab4/ProjetoLab4ApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.lec.springbootlab4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoLab4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
