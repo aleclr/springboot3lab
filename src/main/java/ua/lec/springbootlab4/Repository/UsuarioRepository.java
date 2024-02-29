5262be867a12ab37ebbed1d1763c1b9e712ecaa2
Alec Rondel <alecrondel@hotmail.com>
2024-02-29 00:51:54 +0000

    criando o repository do usuario

--- src/main/java/ua/lec/springbootlab4/Repository/UsuarioRepository.java
+++ src/main/java/ua/lec/springbootlab4/Repository/UsuarioRepository.java
@@ -1,5 +1,13 @@
 package ua.lec.springbootlab4.Repository;
 
-public interface UsuarioRepository {
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import ua.lec.springbootlab4.Entity.Usuario;
+
+//para a função de repository, precisa ser uma interface e precisa extender uma interface do Spring
+//(JpaRepository ou PagingAndSortingRepository)
+public interface UsuarioRepository extends PagingAndSortingRepository<Usuario, Long>{
     
+
+
 }
